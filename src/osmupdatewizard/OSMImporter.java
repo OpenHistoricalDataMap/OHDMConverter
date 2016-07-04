a9bf0461224c73114f32da3c8cbaa61139b70964
Sven <s0545821@htw-berlin.de>
2016-07-04 22:31:29 +0200

    refactored code, outcommented way and relation adding

--- src/osmupdatewizard/OSMImporter.java
+++ src/osmupdatewizard/OSMImporter.java
@@ -94,7 +94,6 @@ public class OSMImporter extends DefaultHandler {
         this.attributes = this.parseAttributes(attributes);
       }
       break; // single original node
-
       case "way": {
         if (status != Status.OUTSIDE) {
           System.err.println("way found but not outside");
@@ -104,7 +103,6 @@ public class OSMImporter extends DefaultHandler {
         this.attributes = this.parseAttributes(attributes);
       }
       break; // original way
-
       case "relation": {
         if (status != Status.OUTSIDE) {
           System.err.println("relation found but not outside");
@@ -114,25 +112,21 @@ public class OSMImporter extends DefaultHandler {
         this.attributes = this.parseAttributes(attributes);
       }
       break; // original relation
-
       case "tag": {
         tagC++;
         this.addTag(this.parseTagAttributes(attributes));
       }
       break; // inside node, way or relation
-
       case "nd": {
         ndC++;
         this.addND(this.parseAttributes(attributes));
       }
       break; // inside a way or relation
-
       case "member": {
         memberC++;
         this.addMember(this.parseAttributes(attributes));
       }
       break; // inside a relation
-
       default:
         otherC++;
         System.out.print(qName + ", ");
@@ -175,33 +169,27 @@ public class OSMImporter extends DefaultHandler {
         }
         this.builder.addNode(this.attributes, this.tags);
         this.status = Status.OUTSIDE;
-
         // cleanup
         this.tags = null;
       }
       break; // single original node
-
       case "way": {
         if (!this.attributes.isEmpty()) {
           this.wA++;
         }
-        this.builder.addWay(this.attributes, this.nds, this.tags);
+        //this.builder.addWay(this.attributes, this.nds, this.tags);
         this.status = Status.OUTSIDE;
-
         // cleanup
         this.tags = null;
         this.nds = null;
       }
-      ;
       break; // original way
-
       case "relation": {
         if (!this.attributes.isEmpty()) {
           this.rA++;
         }
-        this.builder.addRelation(this.attributes, this.members, this.tags);
+        //this.builder.addRelation(this.attributes, this.members, this.tags);
         this.status = Status.OUTSIDE;
-
         // cleanup
         this.tags = null;
         this.nds = null;
@@ -210,14 +198,15 @@ public class OSMImporter extends DefaultHandler {
       break; // original relation
       case "tag":
         break; // inside node, way or relation
-
       case "nd":
         break; // inside a way or relation
-
       case "member":
         break; // inside a relation
       default:
         System.out.print("don't import: " + qName + ", ");
+    }
+    if ((nodeC + wayC + relationC + tagC + ndC + memberC + otherC) % 10000 == 0) {
+      
     }
   }
 

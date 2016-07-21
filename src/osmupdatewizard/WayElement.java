1dd1357425920630b598fde40258ec960fab2e10
Sven <s0545821@htw-berlin.de>
2016-07-21 10:39:28 +0200

    refactored

--- src/osmupdatewizard/WayElement.java
+++ src/osmupdatewizard/WayElement.java
@@ -11,42 +11,40 @@ import java.util.List;
  * @author thsc
  */
 class WayElement extends OSMElement {
-    private final HashSet<NDElement> nds;
 
-    WayElement(ElementStorage storage, HashMap<String, String> attributes, 
-            HashSet<NDElement> nds, HashSet<TagElement> tags) {
-        
-        super(storage, attributes, tags);
-        this.nds = nds;
+  private final HashSet<NDElement> nds;
+
+  public WayElement(ElementStorage storage, HashMap<String, String> attributes,
+          HashSet<NDElement> nds, HashSet<TagElement> tags) {
+    super(storage, attributes, tags);
+    this.nds = nds;
+  }
+
+  public Iterator<NodeElement> getNodes() {
+    List<NodeElement> nodes = new ArrayList<>();
+    ElementStorage storage = this.getStorage();
+
+    // fill list with real nodes instead of placeholders
+    Iterator<NDElement> ndIter = this.nds.iterator();
+    while (ndIter.hasNext()) {
+      NDElement nd = ndIter.next();
+      NodeElement node = storage.getNodeByID(nd.getID());
+      nodes.add(node);
     }
-    
-    Iterator<NodeElement> getNodes() {
-        List<NodeElement> nodes = new ArrayList<>();
-        ElementStorage storage = this.getStorage();
-        
-        // fill list with real nodes instead of placeholders
-        Iterator<NDElement> ndIter = this.nds.iterator();
-        while(ndIter.hasNext()) {
-            NDElement nd = ndIter.next();
-            NodeElement node = storage.getNodeByID(nd.getID());
-            nodes.add(node);
-        }
-        
-        return nodes.iterator();
+    return nodes.iterator();
+  }
+
+  @Override
+  public void print() {
+    System.out.println("Way");
+    super.print();
+    System.out.println("Nodes");
+    Iterator<NodeElement> nodeIter = this.getNodes();
+    while (nodeIter.hasNext()) {
+      NodeElement node = nodeIter.next();
+      node.print();
     }
-    
-    @Override
-    void print() {
-        System.out.println("Way");
-        super.print();
-        
-        System.out.println("Nodes");
-        Iterator<NodeElement> nodeIter = this.getNodes();
-        while(nodeIter.hasNext()) {
-            NodeElement node = nodeIter.next();
-            node.print();
-        }
-        System.out.println("===========================");
-    }
-    
+    System.out.println("===========================");
+  }
+
 }

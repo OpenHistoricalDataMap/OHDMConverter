49e462fccab49af00df4e4fd333d228601004388
Sven <s0545821@htw-berlin.de>
2016-08-06 18:30:55 +0200

    due to changed checking if element exists, empty all tmp storage is not longer neccessary. added print status to summarize update

--- src/osmupdatewizard/ImportCommandBuilder.java
+++ src/osmupdatewizard/ImportCommandBuilder.java
@@ -8,11 +8,12 @@ import java.util.HashSet;
  * @author thsc
  */
 public interface ImportCommandBuilder {
-    public void addNode(HashMap<String, String> attributes, HashSet<TagElement> tags);
 
-    public void addWay(HashMap<String, String> attributes, HashSet<NDElement> nds, HashSet<TagElement> tags);
+  public void addNode(HashMap<String, String> attributes, HashSet<TagElement> tags);
 
-    public void addRelation(HashMap<String, String> attributes, HashSet<MemberElement> members, HashSet<TagElement> tags);
-    
-    public void emptyAllTmpStorage();
+  public void addWay(HashMap<String, String> attributes, HashSet<NDElement> nds, HashSet<TagElement> tags);
+
+  public void addRelation(HashMap<String, String> attributes, HashSet<MemberElement> members, HashSet<TagElement> tags);
+
+  public void printStatus();
 }

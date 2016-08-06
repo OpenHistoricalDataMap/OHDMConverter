1b60ef781e7262ddd5e77f13198587c704eae5a2
Sven <s0545821@htw-berlin.de>
2016-08-06 17:11:04 +0200

    auto format

--- src/osmupdatewizard/NodeElement.java
+++ src/osmupdatewizard/NodeElement.java
@@ -8,19 +8,20 @@ import java.util.HashSet;
  * @author thsc
  */
 public class NodeElement extends OSMElement {
-    NodeElement(HashMap<String, String> attributes, HashSet<TagElement> tags) {
-        super(attributes, tags);
-    }
 
-    NodeElement(ElementStorage storage, HashMap<String, String> attributes, HashSet<TagElement> tags) {
-        super(storage, attributes, tags);
-    }
-    
-    public String getLatitude(){
-      return this.attributes.get("lat");
-    }
-    
-    public String getLongitude(){
-      return this.attributes.get("lon");
-    }
+  NodeElement(HashMap<String, String> attributes, HashSet<TagElement> tags) {
+    super(attributes, tags);
+  }
+
+  NodeElement(ElementStorage storage, HashMap<String, String> attributes, HashSet<TagElement> tags) {
+    super(storage, attributes, tags);
+  }
+
+  public String getLatitude() {
+    return this.attributes.get("lat");
+  }
+
+  public String getLongitude() {
+    return this.attributes.get("lon");
+  }
 }

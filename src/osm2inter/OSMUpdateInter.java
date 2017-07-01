4bc27d50638e1a41eca51127aa059808275385b8
Thomas Schwotzer <thomas.schwotzer@htw-berlin.de>
2017-07-01 10:21:25 +0200

    work on OSM -> OHDM update

--- src/osm2inter/OSMUpdateInter.java
+++ src/osm2inter/OSMUpdateInter.java
@@ -8,22 +8,60 @@ public class OSMUpdateInter {
     
     public static void main(String[] args) {
         // import new osm file into a 'temporary intermediate (TIDB)' db
+
+        // mark all entities which are in IDB but not TIDB as deleted
+        /*
+        use osm_id to figure that out.
+         */
+
+        // remove waynodes / relationsmember entries for deleted entries
+        // remove deleted entries in IDB
         
         // mark all changed entities in 'intermediate' (IDB) as changed
-        
+        /*
+        changed (nodes, ways, relations) if:
+
+        1) Object changed: if
+            a) tags differ (probably object changed)
+            b) classcode differs (probably object changed)
+        2) geometry changed if
+            long/lat changed
+
+        both changed: new
+
+        additionally (ways, relations) if:
+        2.2) geometry changed if
+            node_ids differ
+            -> generate waynodes / relationmember entries
+         */
+
+        // copy all changes from TIDB into IDB
+        // remove changed entries in TIDB
         // remove all unchanged entities in TIDB
         
-        // -> now: TIDB contains new and changed entities
+        // -> now: TIDB contains only new entities
+        /*
+        copy all new entities into IDB and mark as new
+        insert into ?? (select from ??)
+         */
         
-        // mark all new entities in TIB as new (osm_id not in IDB)
-        // now: changed and new can be distinguished
-        
-        // mark all entities in IDB as deleted which are not in TIDB
-        
-        /* -> now:
-        We have identified: new, changed and deleted
-        We haven't yet identified changed relations nodes->ways->relations
-        (only if geometry has changed, not tags) ???
-        */
+        // now: all data in IDB and marked. Old data already removed
+
+        // remove TIDB
+
+        /*
+        update ohdm can do:
+
+        a) increase valid_since for all entries which are not tagged as new or changed
+
+        maybe make a more finegrained decision what tags are to be changed to define a
+        new object (author?)
+
+        b) add new objects (new and both changed, so above)
+
+        c) update geometry
+
+        d) update object
+         */
     }
 }

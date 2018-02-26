c1448867f2c5b87a622a066a68de5ec4a618ce98
thsc42 <thomas.schwotzer@htw-berlin.de>
2018-02-26 14:58:03 +0100

    create a class that test that import / update process

--- src/inter2ohdm/OHDMUpdateInter.java
+++ src/inter2ohdm/OHDMUpdateInter.java
@@ -19,6 +19,7 @@ import util.SQLStatementQueue;
  * 
  */
 public class OHDMUpdateInter {
+    private static final String TAG = "TODO";
 
     public static void main(String args[]) throws IOException, SQLException {
         if(args.length < 3) {
@@ -48,99 +49,135 @@ public class OHDMUpdateInter {
 
         SQLStatementQueue sqlInterUpdate = new SQLStatementQueue(interDBParameters);
 
+        SQLStatementQueue sqlOHDM = new SQLStatementQueue(ohdmParameter);
+
         String interSchema = interDBParameters.getSchema();
         String updateSchema = updateDBParameters.getSchema();
 
-        // mark entries
+        try {
+            // Start update process in intermediate
 
-        SQLStatementQueue sqlOHDM = new SQLStatementQueue(ohdmParameter);
-        
-        /*
+            /* Step 1: mark all entities in intermediate as valid
+            which are unchanged.
 
-        Step 1
-        ++++++
-        mark all unchanged entities
-        
-        set valid flag to false - standard
-update sample_osw.nodes set valid = false;
+            /* First: assume the opposite: whole world has changed
+            and set all valid of all entities to false
+             */
 
-        mark all entities which are unchanged as valid
-        
-update sample_osw.nodes set valid = true where sample_osw.nodes.osm_id IN 
-(select nOld.osm_id from sample_osw.nodes as nOld, sample_osw_new.nodes as nNew where nOld.tstamp = nNew.tstamp AND nOld.osm_id = nNew.osm_id);
-        
-        no remove all entries from old new db
-        
-delete from sample_osw_new.nodes where sample_osw_new.nodes.osm_id IN (select n.osm_id from sample_osw.nodes as n where n.valid);
-        */
-        
-        /*
-        Step 2: 
-        extend time in OHDM for those elements
-        
-        
-        
-        */
-        
-        /*
-        Step 3: 
-        find entities which are in old but not new - those are deleted.
-        Mark as deleted - and delete.
-        
-update sample_osw.nodes set deleted = true where osm_id NOT IN (select osm_id from sample_osw_new.nodes);        
-        
-       remove entries in waynodes and relationmember!!
-        
-        // remove
-        delete from sample_osw.nodes where osm_id NOT IN (select osm_id from sample_osw_new.nodes);
+            System.out.println("set intermediate.nodes.valid to false (assume anything has changed");
+            // NODES
+            // update sample_osw.nodes set valid = false;
+            sqlInterUpdate.append("update ");
+            sqlInterUpdate.append(interSchema);
+            sqlInterUpdate.append(".nodes set valid = false");
 
-        */
-        
-        /*
-        Step 4: 
-        find changes in geometrie and/or objects - see document
-        
-        NODES
-        +++++
-        // find changes in geometry - marked with new tag
-update sample_osw_new.nodes set new = true 
-        where 
-        osm_id IN (select o.osm_id from sample_osw.nodes as o, 
-            sample_osw_new.nodes as n 
-            where o.osm_id = n.osm_id AND (o.longitude != n.longitude OR o.latitude != n.latitude));
-        
-        mark geometry changes to related ways!
-        
-        // find changes in object - marked with changed tag
-update sample_osw_new.nodes set changed = true where osm_id IN (select o.osm_id from sample_osw.nodes as o, sample_osw_new.nodes as n 
-where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
-        
-        WAYS
-        ++++
-        // find changes in geometry - marked with new tag
-TODO        
-        
-        mark relationed relations as changed (new)
-        
-        // find changes in object - marked with changed tag
-update sample_osw_new.ways set changed = true where osm_id IN (select o.osm_id from sample_osw.ways as o, sample_osw_new.ways as n 
-where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
-        
-        RELATIONS
-        +++++++++
-        // find changes in geometry - marked with new tag
-TODO        
-        
-        // find changes in object - marked with changed tag
-update sample_osw_new.relations set changed = true where osm_id IN (select o.osm_id from sample_osw.relations as o, sample_osw_new.relations as n 
-where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
-        
-        
-        */
-        
-        /*
-        Step 5: 
-        handle changed entities - see document
-        */
+            sqlInterUpdate.forceExecute();
+
+            // WAYS
+            // RELATIONS
+
+            /*
+            mark all unchanged entities as valid in intermediate
+            unchanged: time stamps are identical in intermediate an update intermediate db
+
+    update sample_osw.nodes set valid = true where sample_osw.nodes.osm_id IN
+    (select nOld.osm_id from sample_osw.nodes as nOld, sample_osw_new.nodes as nNew where nOld.tstamp = nNew.tstamp AND nOld.osm_id = nNew.osm_id);
+    */
+            sqlInterUpdate.append("update ");
+            sqlInterUpdate.append(interSchema);
+            sqlInterUpdate.append(".nodes set valid = true where ");
+            sqlInterUpdate.append("osm_id IN (");
+            sqlInterUpdate.append("select ");
+            sqlInterUpdate.append("old.osm_id from ");
+            sqlInterUpdate.append(interSchema);
+            sqlInterUpdate.append(".nodes as old, ");
+            sqlInterUpdate.append(updateSchema);
+            sqlInterUpdate.append(".nodes as new where old.tstamp = new.tstamp AND old.osm_id = new.osm_id)");
+
+            sqlInterUpdate.forceExecute();
+    /*
+            no remove all entries from old new db
+
+    delete from sample_osw_new.nodes where sample_osw_new.nodes.osm_id IN (select n.osm_id from sample_osw.nodes as n where n.valid);
+            */
+
+            /*
+            Step 2:
+            extend time in OHDM for those elements
+
+
+
+            */
+
+            /*
+            Step 3:
+            find entities which are in old but not new - those are deleted.
+            Mark as deleted - and delete.
+
+    update sample_osw.nodes set deleted = true where osm_id NOT IN (select osm_id from sample_osw_new.nodes);
+
+           remove entries in waynodes and relationmember!!
+
+            // remove
+            delete from sample_osw.nodes where osm_id NOT IN (select osm_id from sample_osw_new.nodes);
+
+            */
+
+            /*
+            Step 4:
+            find changes in geometrie and/or objects - see document
+
+            NODES
+            +++++
+            // find changes in geometry - marked with new tag
+    update sample_osw_new.nodes set new = true
+            where
+            osm_id IN (select o.osm_id from sample_osw.nodes as o,
+                sample_osw_new.nodes as n
+                where o.osm_id = n.osm_id AND (o.longitude != n.longitude OR o.latitude != n.latitude));
+
+            mark geometry changes to related ways!
+
+            // find changes in object - marked with changed tag
+    update sample_osw_new.nodes set changed = true where osm_id IN (select o.osm_id from sample_osw.nodes as o, sample_osw_new.nodes as n
+    where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
+
+            WAYS
+            ++++
+            // find changes in geometry - marked with new tag
+    TODO
+
+            mark relationed relations as changed (new)
+
+            // find changes in object - marked with changed tag
+    update sample_osw_new.ways set changed = true where osm_id IN (select o.osm_id from sample_osw.ways as o, sample_osw_new.ways as n
+    where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
+
+            RELATIONS
+            +++++++++
+            // find changes in geometry - marked with new tag
+    TODO
+
+            // find changes in object - marked with changed tag
+    update sample_osw_new.relations set changed = true where osm_id IN (select o.osm_id from sample_osw.relations as o, sample_osw_new.relations as n
+    where o.osm_id = n.osm_id AND o.serializedtags != n.serializedtags);
+
+
+            */
+
+            /*
+            Step 5:
+            handle changed entities - see document
+            */
+        }
+        catch(SQLException se) {
+            System.err.println("failure while executing sql statement: " + se.getMessage());
+
+            System.err.println("SQL Inter/Update Queue:");
+            System.err.println(sqlInterUpdate.getCurrentStatement());
+
+            System.err.println("SQL OHDM Queue:");
+            System.err.println(sqlOHDM.getCurrentStatement());
+        }
     }
 }

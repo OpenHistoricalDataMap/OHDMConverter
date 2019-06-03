d44a4f45123d94a457620be6d1b3f208250247b5
kakaoKeks <j.illbruck@googlemail.com>
2019-06-03 13:02:18 +0200

    testordner hinzugefügt

---
+++ test/inter2ohdm/OHDMImporterTest.java
@@ -0,0 +1,14 @@
+package inter2ohdm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OHDMImporterTest {
+
+    @Test
+    public void initialTest(){
+
+    }
+
+}

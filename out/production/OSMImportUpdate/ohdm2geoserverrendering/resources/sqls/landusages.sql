7b62a191942e4cbff5aa1627033f125f1bed209a
Robert Dreyer <s0566997@htw-berlin.de>
2020-07-08 17:10:59 +0200

    started changing ids to sublcassname as identifier

--- out/production/OSMImportUpdate/ohdm2geoserverrendering/resources/sqls/landusages.sql
+++ out/production/OSMImportUpdate/ohdm2geoserverrendering/resources/sqls/landusages.sql
@@ -78,61 +78,6 @@ OR subclassname = 'village_green') as c
  WHERE gg.type_target = 3 AND g.id = gg.id_target AND o.id = gg.id_geoobject_source AND c.id = gg.classification_id;
 
 /* LINES */
-INSERT INTO
-
-my_test_schema.my_landusages(geometry, object_id, geom_id, classid, type, name, valid_since, valid_until, tags, user_id, religion, area, layer, z_order)
-
-SELECT
-
-g.geometry, o.id as object_id, g.id as geom_id, c.id as classid,  c.subclassname, o.name, gg.valid_since,
-gg.valid_until, gg.tags, gg.user_id, gg.religion, St_Area(g.geometry,true), CAST(gg.layer as integer), CAST(gg.z_order as integer)
-
-FROM
-
- (SELECT id, name from ohdm.geoobject) as o,
-
- (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id,
-  tags -> 'religion' as religion,
-  tags -> 'layer' as layer,
-  tags -> 'z_order' as z_order FROM ohdm.geoobject_geometry) as gg,
-
- (SELECT id, line as geometry FROM ohdm.lines) as g,
-
- /* hier jeweils ohdm.polygons, lines, points*/
-
- (SELECT id, subclassname FROM ohdm.classification WHERE subclassname = 'commercial'
-OR subclassname = 'construction'
-OR subclassname = 'industrial'
-OR subclassname = 'residential'
-OR subclassname = 'retail'
-OR subclassname = 'allotments'
-OR subclassname = 'farmland'
-OR subclassname = 'farmyard'
-OR subclassname = 'forest'
-OR subclassname = 'meadow'
-OR subclassname = 'orchard'
-OR subclassname = 'vineyard'
-OR subclassname = 'basin'
-OR subclassname = 'brownfied'
-OR subclassname = 'cementry'
-OR subclassname = 'depot'
-OR subclassname = 'garages'
-OR subclassname = 'grass'
-OR subclassname = 'greenfield'
-OR subclassname = 'greenhous_horticulture'
-OR subclassname = 'landfill'
-OR subclassname = 'military'
-OR subclassname = 'plant_nursery'
-OR subclassname = 'port'
-OR subclassname = 'quarry'
-OR subclassname = 'railway'
-OR subclassname = 'recreation_ground'
-OR subclassname = 'religious'
-OR subclassname = 'reservoir'
-OR subclassname = 'salt_pond'
-OR subclassname = 'village_green') as c
-
- WHERE gg.type_target = 2 AND g.id = gg.id_target AND o.id = gg.id_geoobject_source AND c.id = gg.classification_id;
 
 
 /* POINTS */
--- src/ohdm2geoserverrendering/resources/sqls/waterarea.sql
+++ src/ohdm2geoserverrendering/resources/sqls/waterarea.sql
@@ -24,13 +24,13 @@ FROM
 
  (SELECT id, name from ohdm.geoobject) as o, 
  
- (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry where classification_id = 839) as gg,
+ (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry) as gg,
  
  (SELECT id, polygon as geometry FROM ohdm.polygons WHERE ST_IsValid(polygon) = '1') as g,
  
  /* hier jeweils ohdm.polygons, lines, points*/
  
- (SELECT id, subclassname FROM ohdm.classification where id = 839) as c
+ (SELECT id, subclassname FROM ohdm.classification where subclassname = 'water') as c
  
  WHERE gg.type_target = 3 AND g.id = gg.id_target AND o.id = gg.id_geoobject_source AND c.id = gg.classification_id;
  
@@ -49,13 +49,13 @@ FROM
 
  (SELECT id, name from ohdm.geoobject) as o, 
  
- (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry where classification_id = 839) as gg,
+ (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry) as gg,
  
  (SELECT id, line as geometry FROM ohdm.lines) as g,
  
  /* hier jeweils ohdm.polygons, lines, points*/
  
- (SELECT id, subclassname FROM ohdm.classification where id = 839) as c
+ (SELECT id, subclassname FROM ohdm.classification where subclassname = 'water') as c
  
  WHERE gg.type_target = 2 AND g.id = gg.id_target AND o.id = gg.id_geoobject_source AND c.id = gg.classification_id;
  
@@ -75,13 +75,13 @@ FROM
 
  (SELECT id, name from ohdm.geoobject) as o, 
  
- (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry where classification_id = 839) as gg,
+ (SELECT id_target, classification_id, type_target, id_geoobject_source, valid_since, valid_until, tags, source_user_id as user_id FROM ohdm.geoobject_geometry) as gg,
  
  (SELECT id, point as geometry FROM ohdm.points) as g,
  
  /* hier jeweils ohdm.polygons, lines, points*/
  
- (SELECT id, subclassname FROM ohdm.classification where id = 839) as c
+ (SELECT id, subclassname FROM ohdm.classification where subclassname = 'water') as c
  
  WHERE gg.type_target = 1 AND g.id = gg.id_target AND o.id = gg.id_geoobject_source AND c.id = gg.classification_id;
  

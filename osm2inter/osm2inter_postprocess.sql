265ad1342a6db8b02bef125bfd7d311b2a1d8235
Stefan Sadewasser <62984295+SteSad@users.noreply.github.com>
2022-04-26 13:31:56 +0200

    changed update to diffenrent statements

--- osm2inter/osm2inter_postprocess.sql
+++ osm2inter/osm2inter_postprocess.sql
@@ -1,37 +1,88 @@
 DO $$
 DECLARE t TIMESTAMP := clock_timestamp();
 BEGIN
-    RAISE NOTICE E'\nALTER TABLE creationinformation WITH real primary key\n\n';
     ALTER TABLE inter.creationinformation ADD PRIMARY KEY (id);
-    RAISE NOTICE E'\nTime spent=%\n', clock_timestamp() -t;
+    RAISE NOTICE E'\nALTER TABLE creationinformation WITH real primary key\nTime spent=%\n', clock_timestamp() -t;
 
     SELECT clock_timestamp() INTO t;
-    RAISE NOTICE E'\nINSERT current timestamp into creationinformation\n\n';
     --set timestampstring in creationinformation
     INSERT INTO inter.creationinformation(timestampstring) VALUES (t);
-    RAISE NOTICE E'\nTime spent=%\n\n', clock_timestamp() -t;
+    RAISE NOTICE E'\nINSERT current timestamp into creationinformation\nTime spent=%\n\n', clock_timestamp() -t;
 
     SELECT clock_timestamp() INTO t;
-    RAISE NOTICE E'\nALTER all other tables WITH real primary key\n\n';
     -- alter tables with primary keys
     ALTER TABLE inter.nodes ADD PRIMARY KEY (id);
     ALTER TABLE inter.ways ADD PRIMARY KEY (id);
     ALTER TABLE inter.waynodes ADD PRIMARY KEY (id);
     ALTER TABLE inter.relations ADD PRIMARY KEY (id);
     ALTER TABLE inter.relationmembers ADD PRIMARY KEY (id);
-    RAISE NOTICE E'\nTime spent=%\n\n', clock_timestamp() -t;
+    RAISE NOTICE E'\nALTER all other tables WITH real primary key\nTime spent=%\n\n', clock_timestamp() -t;
+
 
     SELECT clock_timestamp() INTO t;
-    RAISE NOTICE E'\nUPDATE osm_object_mapfeatures with feature_value exists\n\n';
-    UPDATE inter.osm_object_mapfeatures AS f 
-    SET feature_id = (SELECT cla.id FROM misc.classification AS cla WHERE cla."class" = f.feature_key AND cla.subclassname = f.feature_value)
-    WHERE f.feature_value IN (SELECT cla.subclassname FROM misc.classification AS cla WHERE cla."class" = f.feature_key);
-    RAISE NOTICE E'\nTime spent=%\n\n', clock_timestamp() -t;
+    -- admin_level BETWEEN 1 - 12
+    UPDATE inter.osm_object_mapfeatures AS features SET feature_id =
+    (
+        SELECT cla.id FROM inter.osm_object_mapfeatures AS oom, misc.classification AS cla
+        WHERE
+            oom.osm_id = features.osm_id AND
+	        cla.classname LIKE 'ohdm%' AND 
+	        oom.feature_key LIKE 'admin%' AND
+            oom.feature_value ~ '[1-9]?[0-2]?' AND
+            cla.subclassname = CONCAT('adminlevel_', feature_value)
+    );
 
+    -- admin_level undefined
+    UPDATE inter.osm_object_mapfeatures AS features SET feature_id =
+    (
+        SELECT cla.id FROM inter.osm_object_mapfeatures AS oom, misc.classification AS cla
+        WHERE
+            oom.osm_id = features.osm_id AND
+	        cla.classname LIKE 'ohdm%' AND 
+	        oom.feature_key LIKE 'admin%' AND
+            cla.subclassname = 'undefined'
+    );
+
+    -- mapfeature and subfeature exists
+    UPDATE inter.osm_object_mapfeatures AS features SET feature_id =
+    (
+        SELECT cla.id FROM inter.osm_object_mapfeatures AS oom, misc.classification AS cla
+        WHERE
+            oom.osm_id = features.osm_id AND
+            cla.classname = oom.feature_key AND
+	        cla.subclassname = oom.feature_value
+    );
+    
+    -- mapfeature and subfeature is undefined
+    UPDATE inter.osm_object_mapfeatures AS features SET feature_id =
+    (   SELECT cla.id FROM inter.osm_object_mapfeatures AS oom, misc.classification AS cla
+        WHERE
+            oom.osm_id = features.osm_id AND
+            cla.classname = oom.feature_key AND
+            oom.feature_value NOTNULL AND
+	        cla.subclassname = 'undefined'
+    );
+    
+    RAISE NOTICE E'\nUPDATE osm_object_mapfeatures\nTime spent=%\n\n', clock_timestamp() -t;
+    
     SELECT clock_timestamp() INTO t;
-    RAISE NOTICE E'\nUPDATE osm_object_mapfeatures with undefined subclass\n\n';
-    UPDATE inter.osm_object_mapfeatures AS f 
-    SET feature_id = (SELECT cla.id FROM misc.classification AS cla WHERE cla."class" = f.feature_key AND cla.subclassname = 'undefined') 
-    WHERE f.feature_value NOT IN (SELECT cla.subclassname FROM misc.classification AS cla WHERE cla."class" = f.feature_key);
-    RAISE NOTICE E'\nTime spent=%\n\n', clock_timestamp() -t;
+
+    -- node.osm_id exist in cross-reference table
+    UPDATE inter.nodes AS nodes SET mapfeatures_ids = 
+    (
+        SELECT oom.feature_id FROM inter.nodes AS n ,inter.osm_object_mapfeatures AS oom
+        WHERE nodes.osm_id = n.osm_id = oom.osm_id
+    );
+
+    -- node.osm_id exist in cross-reference table
+    UPDATE inter.nodes AS n SET mapfeatures_ids = 
+    (
+        CASE WHEN n.osm_id NOT IN (SELECT osm_id FROM inter.osm_object_mapfeatures) THEN '-1' END
+    );
+
+
+    RAISE NOTICE E'\nUPDATE mapfeatures_ids\nTime spent=%\n\n', clock_timestamp() -t;
+
+
 END;
+$$

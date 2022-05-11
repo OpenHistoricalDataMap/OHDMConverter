b81eb46dad0f32845550d2549df9e0764ba7ed5c
Stefan Sadewasser <62984295+SteSad@users.noreply.github.com>
2022-05-11 15:28:46 +0200

    may finished docu

--- inter2ohdm/inter2ohdm.sh
+++ inter2ohdm/inter2ohdm.sh
@@ -2,7 +2,7 @@
 full_file_name=$(realpath -e $0)
 path=${full_file_name%/*} 
 t1=$(date +%s)
-sudo -iu postgres psql -d ohdm -f $path'/inter2ohdm.sql'
+psql -W -d ohdm -f $path'/inter2ohdm.sql'
 sleep 3
 t2=$(date +%s)
 duration=$((t2 - t1))

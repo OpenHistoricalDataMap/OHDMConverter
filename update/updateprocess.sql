d26a23e2be431444bd384fad2e3bf1acce234640
SteSad <stefan.sadewasser@gmx.de>
2022-06-01 10:13:30 +0200

    added insertion on deleted objects from inter in updatedb
    added extension pg_trgm to compare string similarity

--- update/updateprocess.sql
+++ update/updateprocess.sql
@@ -113,19 +113,46 @@ BEGIN
     SET object_changed = true
     FROM inter.nodes AS nodes
     WHERE updatenodes.osm_id = nodes.osm_id 
-    AND NOT updatenodes.serializedtags = nodes.serializedtags;
+    AND 
+    (
+        NOT updatenodes.serializedtags = nodes.serializedtags OR
+        NOT updatenodes.tstamp = nodes.tstamp OR
+        similarity(updatenodes.mapfeature_ids,nodes.mapfeature_ids) < 1 OR
+        NOT updatenodes.uid = nodes.uid OR
+        NOT updatenodes.username = nodes.username OR
+        NOT updatenodes.name = nodes.name OR
+        NOT updatenodes.url = nodes.url
+    );
 
     UPDATE updatedb.ways as updateways
     SET object_changed = true
     FROM inter.ways AS ways
     WHERE updateways.osm_id = ways.osm_id 
-    AND NOT updateways.serializedtags = ways.serializedtags;
+    AND 
+    (
+        NOT updateways.serializedtags = ways.serializedtags OR
+        NOT updateways.tstamp = ways.tstamp OR
+        similarity(updateways.mapfeature_ids,ways.mapfeature_ids) < 1 OR
+        NOT updateways.uid = ways.uid OR
+        NOT updateways.username = ways.username OR
+        NOT updateways.name = ways.name OR
+        NOT updateways.url = ways.url
+    );
 
     UPDATE updatedb.relations as updaterelations
     SET object_changed = true
     FROM inter.relations AS relations
     WHERE updaterelations.osm_id = relations.osm_id 
-    AND NOT updaterelations.serializedtags = relations.serializedtags;
+    AND 
+    (
+        NOT updaterelations.serializedtags = relations.serializedtags OR
+        NOT updaterelations.tstamp = relations.tstamp OR
+        similarity(updaterelations.mapfeature_ids,relations.mapfeature_ids) < 1 OR
+        NOT updaterelations.uid = relations.uid OR
+        NOT updaterelations.username = relations.username OR
+        NOT updaterelations.name = relations.name OR
+        NOT updaterelations.url = relations.url
+    );
 
 END;
 $$;
@@ -145,6 +172,108 @@ BEGIN
     UPDATE updatedb.relations
     SET object_new = true
     WHERE osm_id NOT IN (SELECT osm_id FROM inter.relations);
+
+END;
+$$;
+
+DO $$
+BEGIN
+    RAISE NOTICE E'Insert delete objects in updatedb'; 
+    INSERT INTO updatedb.nodes (
+        osm_id,
+        tstamp,
+        mapfeature_ids,
+        serializedtags,
+        geom,
+        uid,
+        username,
+        name,
+        url,
+        geom_changed,
+        object_changed,
+        deleted,
+        object_new,
+        has_name,
+        valid 
+    )
+    (
+        SELECT osm_id,tstamp,mapfeature_ids,serializedtags,geom,uid,username,name,url,geom_changed,object_changed,true,object_new,has_name,valid 
+        FROM inter.nodes AS nodes
+        WHERE nodes.osm_id NOT IN (
+            SELECT osm_id FROM updatedb.nodes
+        )
+    );
+
+    INSERT INTO updatedb.ways (
+        osm_id,
+        tstamp,
+        mapfeature_ids,
+        serializedtags,
+        geom,
+        uid,
+        username,
+        name,
+        url,
+        member,
+        geom_changed,
+        object_changed,
+        deleted,
+        object_new,
+        has_name,
+        valid 
+    )
+    (
+        SELECT osm_id,tstamp,mapfeature_ids,serializedtags,geom,uid,username,name,url,member,geom_changed,object_changed,true,object_new,has_name,valid 
+        FROM inter.ways AS ways
+        WHERE ways.osm_id NOT IN (
+            SELECT osm_id FROM updatedb.ways
+        )
+    );
+
+    INSERT INTO updatedb.relations (
+        osm_id,
+        tstamp,
+        mapfeature_ids,
+        serializedtags,
+        geom,
+        uid,
+        username,
+        name,
+        url,
+        member,
+        geom_changed,
+        object_changed,
+        deleted,
+        object_new,
+        has_name,
+        valid 
+    )
+    (
+        SELECT osm_id,tstamp,mapfeature_ids,serializedtags,geom,uid,username,name,url,member,geom_changed,object_changed,true,object_new,has_name,valid 
+        FROM inter.relations AS relations
+        WHERE relations.osm_id NOT IN (
+            SELECT osm_id FROM updatedb.relations
+        )
+    );
+
+END;
+$$;
+
+DO $$
+BEGIN
+    RAISE NOTICE E'Mark all changed or new objects as not valid'; 
+
+    UPDATE updatedb.nodes
+    SET valid = false
+    WHERE geom_changed = true or object_changed = true or object_new = true;
+
+    UPDATE updatedb.ways
+    SET valid = false
+    WHERE geom_changed = true or object_changed = true or object_new = true;
+
+    UPDATE updatedb.relations
+    SET valid = false
+    WHERE geom_changed = true or object_changed = true or object_new = true;
 
 END;
 $$;

90942173cc76185829e46af0155dcf546937d655
SteSad <stefan.sadewasser@gmx.de>
2022-06-08 16:03:01 +0200

    changed convert script for osm2inter

--- osm2inter/osm2inter.sh
+++ osm2inter/osm2inter.sh
@@ -10,11 +10,10 @@ psql -d ohdm -c "\copy inter.classification TO "$classification_file" WITH DELIM
 printf "Saved mapfeatures into %s\n\n" "$classification_file"
 
 
-osm2pgsql -d ohdm -x -O flex -S $path'/osm2inter.lua' -c $path'/map.osm'
-printf "Converted osm2inter\n"
+osm2pgsql -d ohdm -x -O flex -S $path'/osm2inter.lua' -c $path'/testmap.osm'
+printf "Converted osm2inter\n\n"
 sleep 3
 psql -d ohdm -f $path'/postprocess.sql'
-printf "Postprocess done\n"
 t2=$(date +%s)
 duration=$((t2 - t1))
 printf "Duration Time(H:M:S): %s" "$(date -d @${duration} -u +%H:%M:%S)"

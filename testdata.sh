16cfe85d6255a44bbd5dee6a08d9527bf80bc851
SteSad <stefan.sadewasser@gmx.de>
2022-06-09 15:15:15 +0200

    added bash script to test all scripts togehter

---
+++ testdata.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+full_file_name=$(realpath -e $0)
+path=${full_file_name%/*} 
+$path'/osm2inter/osm2inter.sh'
+
+$path'/inter2ohdm/inter2ohdm.sh'
+
+$path'/update/osm2updatedb.sh'
+
+psql -d ohdm -f $path'/update/04updateprocess.sql'
+
+psql -d ohdm -f $path'/update/05test_insert-in-ohdm.sql'
+
+psql -d ohdm -f $path'/update/06alter-inter-schema.sql'

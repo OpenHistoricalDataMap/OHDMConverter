34267f7b03a0bec8fe7976a2426f1e137d028d6f
SteSad <stefan.sadewasser@gmx.de>
2022-06-09 15:03:50 +0200

    added bash script for the converting method osm2inter

--- osm2inter/osm2inter.sh
+++ osm2inter/osm2inter.sh
@@ -2,18 +2,18 @@
 full_file_name=$(realpath -e $0)
 path=${full_file_name%/*} 
 t1=$(date +%s)
-psql -d ohdm -f $path'/preprocess.sql'
+psql -d ohdm -f $path'/01preprocess.sql'
 sleep 3
 
-classification_file=$path"/classification.csv"
+classification_file=$path"/02classification.csv"
 psql -d ohdm -c "\copy inter.classification TO "$classification_file" WITH DELIMITER ',';" 
 printf "Saved mapfeatures into %s\n\n" "$classification_file"
 
 
-osm2pgsql -d ohdm -x -O flex -S $path'/osm2inter.lua' -c $path'/testmap.osm'
+osm2pgsql -d ohdm -x -O flex -S $path'/02osm2inter.lua' -c $path'/02testmap.osm'
 printf "Converted osm2inter\n\n"
 sleep 3
-psql -d ohdm -f $path'/postprocess.sql'
+psql -d ohdm -f $path'/03postprocess.sql'
 t2=$(date +%s)
 duration=$((t2 - t1))
 printf "Duration Time(H:M:S): %s" "$(date -d @${duration} -u +%H:%M:%S)"

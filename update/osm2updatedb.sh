7515b69a3fbe564248b47646da0926d85008f7f4
SteSad <stefan.sadewasser@gmx.de>
2022-06-09 15:04:48 +0200

    added bash script for the converting method osm2inter on updatedb

---
+++ update/osm2updatedb.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+full_file_name=$(realpath -e $0)
+path=${full_file_name%/*} 
+t1=$(date +%s)
+psql -d ohdm -f $path'/01preprocess.sql'
+sleep 3
+
+classification_file=$path"/02classification.csv"
+psql -d ohdm -c "\copy updatedb.classification TO "$classification_file" WITH DELIMITER ',';" 
+printf "Saved mapfeatures into %s\n\n" "$classification_file"
+
+
+osm2pgsql -d ohdm -x -O flex -S $path'/02osm2updatedb.lua' -c $path'/02testupdate.osm'
+printf "Converted osm2updatedb\n"
+sleep 3
+psql -d ohdm -f $path'/03postprocess.sql'
+printf "Postprocess done\n"
+t2=$(date +%s)
+duration=$((t2 - t1))
+printf "Duration Time(H:M:S): %s" "$(date -d @${duration} -u +%H:%M:%S)"
